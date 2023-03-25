a0c63593b1f0b1ebb0811e7b2b0778adb7de03e8
evgenii <evgenii@marketmuse.com>
2023-03-25 20:10:58 +0400

    Add some simple methods

--- lib/polyomino_puzzle.ex
+++ lib/polyomino_puzzle.ex
@@ -27,6 +27,20 @@ defmodule PolyominoPuzzle do
         [[3, 'r'], [1, 'l'], [1, 'd']],
         [[1, 'r'], [1, 'd']]
     ]
+  end
 
+  def get_n_random_figures(n, figures) do
+    {
+      Enum.slice(shuffle_figures(figures), 0..n-1),
+      Enum.slice(shuffle_figures(figures), n..-1)
+    }
+  end
+
+  def shuffle_figures(figures) do
+    Enum.shuffle(figures)
+  end
+
+  def contains?(figures, figure) do
+    Enum.member?(figures, figure)
   end
 end
