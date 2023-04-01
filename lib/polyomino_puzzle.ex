0cb2a2995a24cbabe5a8e7a7d6466f62eabcd95b
evgenii <evgenii@marketmuse.com>
2023-04-01 20:56:22 +0400

    Add save and load methods

--- lib/polyomino_puzzle.ex
+++ lib/polyomino_puzzle.ex
@@ -29,7 +29,7 @@ defmodule PolyominoPuzzle do
     ]
   end
 
-  def get_n_random_figures(n, figures) do
+  def get_n_random_figures(figures, n) do
     {
       Enum.slice(shuffle_figures(figures), 0..n-1),
       Enum.slice(shuffle_figures(figures), n..-1)
@@ -40,7 +40,25 @@ defmodule PolyominoPuzzle do
     Enum.shuffle(figures)
   end
 
+  def create_init_and_free_figure_sets(init_set_size) do
+    PolyominoPuzzle.create_figures
+    |> PolyominoPuzzle.shuffle_figures
+    |> PolyominoPuzzle.get_n_random_figures(init_set_size)
+  end
+
   def contains?(figures, figure) do
     Enum.member?(figures, figure)
+  end
+
+  def save(figures, filename) do
+    binary = :erlang.term_to_binary(figures)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _reason} -> "That file does not exist"
+    end
   end
 end
