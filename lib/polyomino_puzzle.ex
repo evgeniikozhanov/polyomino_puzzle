92dad666e9cee4c2d20b9052b7c69f5e571d5394
evgenii <evgenii@marketmuse.com>
2023-04-02 17:16:30 +0400

    Add a new doctest

--- lib/polyomino_puzzle.ex
+++ lib/polyomino_puzzle.ex
@@ -1,16 +1,10 @@
 defmodule PolyominoPuzzle do
   @moduledoc """
-  Documentation for PolyominoPuzzle.
+  Provide methods for creating and handling polyomino puzzle game
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> PolyominoPuzzle.hello()
-      :world
-
+  Get hardcoded figures that we use in this game
   """
   def create_figures do
     [
@@ -29,6 +23,19 @@ defmodule PolyominoPuzzle do
     ]
   end
 
+  @doc """
+     Divides figures into two heaps: known heap (that are placed on
+     the game field) and user heap (that a player has to place on
+     the game field in the game process).
+     The `n` argument indicates size of the known heap.
+
+   Examples
+
+      iex> figures = PolyominoPuzzle.create_figures()
+      iex> {known_figs, player_figs} = PolyominoPuzzle.get_n_random_figures(figures, 3)
+      iex> length(known_figs)
+      3
+  """
   def get_n_random_figures(figures, n) do
     {
       Enum.slice(shuffle_figures(figures), 0..n-1),
@@ -46,6 +53,13 @@ defmodule PolyominoPuzzle do
     |> PolyominoPuzzle.get_n_random_figures(init_set_size)
   end
 
+  @doc """
+    Determine whether figures contains a given figure
+
+  ## Examples
+    iex> PolyominoPuzzle.contains?([[1, 2], [1, 3]], [1, 2])
+    true
+  """
   def contains?(figures, figure) do
     Enum.member?(figures, figure)
   end
